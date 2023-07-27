04e90249679a9b7050541bcc8524566edd09fe0d
Vasunamdev <116533893+Vasunamdev@users.noreply.github.com>
2023-07-27 11:58:17 +0530

    Add files via upload

---
+++ 148. Sort List.cpp
@@ -0,0 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL && list2==NULL)
+        return NULL;
+        else if(list1==NULL && list2!=NULL)
+        return list2;
+        else if(list2==NULL && list1!=NULL)
+        return list1;
+      ListNode *temp=list1, *temp2=list2, *ans=NULL,*prev;
+      if(temp->val<temp2->val)
+          {    ans=temp;
+               prev=temp;
+              temp=temp->next;
+          }
+          else
+          {   ans=temp2;
+              prev=temp2;
+              temp2=temp2->next;
+          }
+
+      while(temp!=NULL || temp2!=NULL)
+      {
+          if(temp!=NULL && temp2!=NULL && temp->val<temp2->val  )
+          {   prev->next=temp;
+               prev=temp;
+              temp=temp->next;
+          }
+          else if(temp2!=NULL && temp!=NULL && temp->val>=temp2->val )
+          {
+              prev->next=temp2;
+              prev=temp2;
+              temp2=temp2->next;
+          }
+          else if(temp==NULL && temp2!=NULL)
+          {
+              prev->next=temp2;
+              prev=temp2;
+              temp2=temp2->next;
+          }
+          else if(temp2==NULL && temp!=NULL)
+          {
+              prev->next=temp;
+              prev=temp;
+              temp=temp->next;
+          }
+
+      }  
+      return ans;
+    }
+    ListNode* sortList(ListNode* head) {
+           if(head==NULL || head->next==NULL)
+        return head;
+    ListNode *temp=head, *t2=head, *prev;
+   
+    while(head!=NULL)
+    {
+       if(head->next==NULL)
+       head=head->next;
+       else
+       {
+          head=head->next->next;
+          prev=temp;
+          temp=temp->next;
+       }
+    } 
+    prev->next=NULL;
+    ListNode *l1=sortList(t2);
+    ListNode *l2=sortList(temp);
+    return mergeTwoLists(l1,l2);
+    }
+};
